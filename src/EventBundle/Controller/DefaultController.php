d48644b50e49a655990ccaae71e7e4ab62faff1c
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-11-10 13:58:34 +0100

    render first views

--- src/EventBundle/Controller/DefaultController.php
+++ src/EventBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction($firstName)
     {
-        return $this->render('EventBundle:Default:index.html.twig');
+        return $this->render('EventBundle:Default:index.html.twig', [
+            'name' => $firstName
+        ]);
+    }
+
+    public function testAction($firstName)
+    {
+        $arr = ['name' => $firstName];
+        $response = new Response(json_encode($arr));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }

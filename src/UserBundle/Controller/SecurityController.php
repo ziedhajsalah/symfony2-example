7848a1c8273c37e41f22c9970be5637335aad4cb
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-11-12 00:37:53 +0100

    add login to the app

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login_form")
+     *
+     * @Template()
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUserName = $authenticationUtils->getLastUsername();
+
+        return [
+            'last_username' => $lastUserName,
+            'error'          => $error
+        ];
+    }
+
+    /**
+     * @Route("/login_check", name="login_check")
+     */
+    public function loginCheckAction()
+    {
+
+    }
+}

875dc31309423e4631dd33ff7b657796340378f3
Zied Ben Haj Salah <ziedhajsalah@gmail.com>
2016-11-13 14:48:28 +0100

    create RegisterController with registerAction and render a form in the template

---
+++ src/UserBundle/Controller/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     * @Template()
+     */
+    public function registerAction()
+    {
+        $form = $this->createFormBuilder()
+            ->add('username', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->getForm();
+
+        return ['form' => $form];
+    }
+}
